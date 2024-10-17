03a1a9feffacf73e89ff1a66ea4c0a1b7961997a
tapu0411 <mtapas.mohanty95@gmail.com>
2024-10-17 12:23:05 +0530

    added changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 import { createServer } from "http";
 import cors from "cors";
 
-const port = 4000;
+const port = 4000 ;
 const app = express();
 const server = createServer(app);
 
@@ -47,55 +47,148 @@ io.on('connection', (socket) => {
             console.log(`${userId} connected with socketId: ${socket.id}`);
           })
   
-          let username = null
+          
           socket.on("subscribe", (data)=>{
   
               const roomId = data.roomId;
-              const username = data.username;
+              const userId = data.userId;
               const otherUserId = data.otherUserId;
-              console.log("subscribe", roomId, username)
+              console.log("subscribe", roomId, userId)
               socket.join(`${roomId}`);
   
               io.emit("otherUserId", otherUserId, roomId)
-              io.to(`${roomId}`).emit("otherUser",username)
+              io.to(`${roomId}`).emit("otherUser",userId)
           })
+
+          socket.on("OtherUserConnectedRevert", (data)=>{
+
+            const roomId = data.roomId
+            const userId = data.userId;
+            const otherUserId = data.otherUserId;
+
+
+            console.log("OtherUserConnectedRevert", data)
+            io.emit("otherUserIdRevert", data)
+
+          });
+
+
+
           socket.on("newMessage", (data)=>{
   
-              const roomId =data.roomId;
-              const messageContent = data.messageContent;
-              const messageTimestamp = data.messageTimestamp;
-              const userId = data.userId;
-              const otherUserId = data.otherUserId;
-              const isRead = false;
+              const roomId =data.groupId;
+              const messageContent = data.commenttext;
+              const messageTimestamp = data.commenttime;
+              const userId = data.commenter;
+              const isRead = data.read;
+              const file  = data.file;
+              const groupType = data.groupType;
+              const senderId = data.senderId
+              
               // const chat = await addMessageHelper(roomId, username, messageContent, messageTimestamp, isRead)
-              console.log(data)
+              console.log("NewMessage", data)
               io.in(`${roomId}`).emit("newMessage", data)
               // socket.broadcast.to(`${roomId}`).emit("newMessage", data)
-          })
+          });
+
+          // socket.on("newMessageOuter", (data)=>{
+
+          //   const roomId =data.groupId;
+          //   const messageContent = data.commenttext;
+          //   const messageTimestamp = data.commenttime;
+          //   const userId = data.commenter;
+          //   const isRead = data.read;
+              
+          //   console.log("NewMessageOuter", data)
+          //   io.in(`${roomId}`).emit("newMessage", data)
+
+          // });
   
-          socket.on('typing', (data) => {
-            const { roomId, typingUserId } = data;
+
+
+
+        socket.on('typing', (data) => {
+          const { roomId, typingUserId, otherUserId} = data;
+          if(roomId && typingUserId) {
+              
+            console.log("roomId typing", roomId, typingUserId);
+              socket.to(`${data.roomId}`).emit("typing",data);
+              console.log("who is typing 128",data);
+              
+              if (users[otherUserId]) {
+                  io.to(otherUserId).emit("typing",data);
+                  console.log("Typing event emitted to particular userId using SocketID: ", data+" "+users[otherUserId ]);
+              }
+              else {
+                  console.log(`User with userId: ${otherUserId} is not connected`);
   
-            if (roomId && typingUserId) {
-                console.log("roomId typing", roomId);
-                io.in(`${roomId}`).emit("typing", data.typingUserId);
-            } else {
-                console.log("Invalid data for typing event", data);
-            }
+              }
+          } else {
+              console.log("Invalid data for typing event", data);
+          }
+      });
+
+        socket.on('markMessageRead', (data) => {
+          const room = data.roomId
+          const selfId = data.selfId
+          const otherId = data.otherId 
+          console.log("markMessageReadEvent emitted by ", data.selfId)
+          io.in(`${room}`).emit("markMessageRead", data);
+        })  
+
+        socket.on('onChatSeen', (data) =>  {
+          const roomId = data.roomId;
+          const selfId = data.selfId;
+          const otherId = data.otherUserId
+
+          console.log("onChatSeen", roomId, selfId )
+          // Emit the id of the person who's on chat screen
+          io.in(`${roomId}`).emit("onChatSeen", selfId);
+        
+        });
+
+        socket.on('onChatSeenLeft', (data) => {
+
+          const roomId = data.roomId
+          const selfId = data.selfId
+
+          // Emit the id of the person leaving the chat screen in the room
+          io.in(`${roomId}`).emit("onChatSeenLeft", data.selfId)
+        });
+
+
+        socket.on("chatAttachSent", (data) => {
+
+          const roomId = data.roomId;
+          const senderId = data.senderId
+
+          console.log("chatAttachSent", data);
+
+          // Emit the id of the person leaving the chat screen in the room
+          io.in(`${roomId}`).emit("chatAttachSent", data)
         });
 
         socket.on("unsubscribe", (data) => {
               const roomId = data.roomId;
-              console.log("unsubscribe", roomId);
+              const userId = data.userId;
 
-              // Define a proper message to emit here if needed, e.g., a notification message.
-              const leaveMessage = {
-                      message: `${username || 'A user'} has left the room.`,
-                      timestamp: new Date(),
-       };
+              console.log("unsubscribe", roomId + "by user: " + userId + "at " + new Date());
 
-      io.in(`${roomId}`).emit("newMessage", leaveMessage); // Emit a relevant message instead of chat.
-      socket.leave(`${roomId}`);
+              // Emit a relevant message
+              socket.to(roomId).emit("userLeft", "User left with id: " + userId);
+
+              socket.leave(`${roomId}`);
+        });
+
+
+      // Notices 
+      socket.on('noticePost', (data)=>{
+
+        const uploaderId = data.uploaderId          
+
+
+
+
       });
 
 
@@ -110,7 +203,7 @@ io.on('connection', (socket) => {
             }
           }
       });
-
+ 
 });
 
 server.listen(port, '0.0.0.0', () => {
